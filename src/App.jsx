44a1b122277e9ed0e331bdec8f09651e14fc4b6a
mostafa <mostafameerzad@gmail.com>
2023-01-01 14:01:00 +0430

    Complete making Rating and Thanks

--- src/App.jsx
+++ src/App.jsx
@@ -1,133 +1,110 @@
-// import React, { useState } from "react";
-// // import {useEffect} from 'react'
-// // import {Accordion} from "react-headless-accordion";
-// // import 'react-accessible-accordion/dist/fancy-example.css';
-// import {
-//   Accordion,
-//   AccordionItem,
-//   AccordionItemButton,
-//   AccordionItemPanel,
-// } from "react-accessible-accordion";
-// // import "react-a"
-// import "./style.css";
-// import { motion } from "framer-motion";
-// // import AccordionItemButtonWrapper from "react-accessible-accordion/dist/types/components/AccordionItemButton";
-// const App = () => {
-//   // useEffect(() => {
-//   //   console.log('useEffect called ');
-//   //   fetch("http://localhost:3000/posts").then(res=>res.json()).then((data) => {
-//   //     console.log(data[0]);
-//   //   })
-//   // })
-
-//   const [test, setTest] = useState(0);
-//   const handleChange = (e) => {
-//     setTest(e.target.value);
-//   };
-//   // return (<Accordion className={"my-accordion"} allowZeroExpanded={true}>
-//   //     <AccordionItem className={"my-accordion__item"} activeClassName={"active"}>
-//   //         <h2>Accordion Item</h2>
-
-//   //         <AccordionItemButton className={"my-accordion__button"}>
-//   //             accordion button
-//   //         </AccordionItemButton>
-
-//   //         <AccordionItemPanel className="my-accordion__panel">
-//   //             <p>
-//   //                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
-//   //                 accusamus ad architecto deleniti dicta id illo illum, in minima modi
-//   //                 necessitatibus pariatur velit voluptates. A accusamus commodi eaque
-//   //                 libero non omnis vel veniam voluptatem? Architecto aut cupiditate
-//   //                 delectus dolor doloribus ex exercitationem, facilis incidunt iure,
-//   //                 nesciunt non, praesentium provident quo sit temporibus? Ab,
-//   //                 consequuntur culpa dicta harum incidunt ipsum mollitia quas repellat
-//   //                 sequi? Ad autem commodi, consectetur debitis delectus eos
-//   //                 exercitationem illum incidunt ipsam ipsum libero magni nesciunt
-//   //                 nihil officia perferendis qui quisquam quos repellendus saepe
-//   //                 voluptate? Alias asperiores autem dolorem iusto, modi nulla odit
-//   //                 officiis perspiciatis repellat soluta tempora.
-//   //             </p>
-//   //         </AccordionItemPanel>
-//   //     </AccordionItem>
-//   //     <AccordionItem className={"my-accordion__item"} activeClassName={"active"}>
-
-//   //         <h2>Accordion Item</h2>
-
-//   //         <AccordionItemButton className={"my-accordion__button"}>
-//   //             accordion button
-//   //         </AccordionItemButton>
-
-//   //         <AccordionItemPanel className="my-accordion__panel">
-//   //             <p>
-//   //                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
-//   //                 accusamus ad architecto deleniti dicta id illo illum, in minima modi
-//   //                 necessitatibus pariatur velit voluptates. A accusamus commodi eaque
-//   //                 libero non omnis vel veniam voluptatem? Architecto aut cupiditate
-//   //                 delectus dolor doloribus ex exercitationem, facilis incidunt iure,
-//   //                 nesciunt non, praesentium provident quo sit temporibus? Ab,
-//   //                 consequuntur culpa dicta harum incidunt ipsum mollitia quas repellat
-//   //                 sequi? Ad autem commodi, consectetur debitis delectus eos
-//   //                 exercitationem illum incidunt ipsam ipsum libero magni nesciunt
-//   //                 nihil officia perferendis qui quisquam quos repellendus saepe
-//   //                 voluptate? Alias asperiores autem dolorem iusto, modi nulla odit
-//   //                 officiis perspiciatis repellat soluta tempora.
-//   //             </p>
-//   //         </AccordionItemPanel>
-
-//   //     </AccordionItem>
-//   //     <AccordionItem className={"my-accordion__item"} activeClassName={"active"}>
-//   //         <h2>Accordion Item</h2>
-
-//   //         <AccordionItemButton className={"my-accordion__button"}>
-//   //             accordion button
-//   //         </AccordionItemButton>
-
-//   //         <AccordionItemPanel className="my-accordion__panel">
-//   //             <p>
-//   //                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
-//   //                 accusamus ad architecto deleniti dicta id illo illum, in minima modi
-//   //                 necessitatibus pariatur velit voluptates. A accusamus commodi eaque
-//   //                 libero non omnis vel veniam voluptatem? Architecto aut cupiditate
-//   //                 delectus dolor doloribus ex exercitationem, facilis incidunt iure,
-//   //                 nesciunt non, praesentium provident quo sit temporibus? Ab,
-//   //                 consequuntur culpa dicta harum incidunt ipsum mollitia quas repellat
-//   //                 sequi? Ad autem commodi, consectetur debitis delectus eos
-//   //                 exercitationem illum incidunt ipsam ipsum libero magni nesciunt
-//   //                 nihil officia perferendis qui quisquam quos repellendus saepe
-//   //                 voluptate? Alias asperiores autem dolorem iusto, modi nulla odit
-//   //                 officiis perspiciatis repellat soluta tempora.
-//   //             </p>
-//   //         </AccordionItemPanel>
-//   //     </AccordionItem>
-//   // </Accordion>
-
-//   // <form action="#">
-//   //     <h1>a form for testing purposes</h1>
-//   //     <h2>{window.innerWidth}</h2>
-//   //     {/*<div className="box"/>*/}
-//   //     <label htmlFor="test">enter your value</label>
-//   //     <input type="number" name="testValue" id="test" value={test} min={10} max={100} placeholder={"$"}
-//   //            onChange={handleChange}/>
-//   //     <input type="submit" value="continue"/>
-//   // </form>
-
-//   // );
-
-//   // return <motion.div  >lorem</motion.div>;
-
-//   return <AnimateText/>
-// };
-
-// export default App;
+import React from "react";
+import { useState } from "react";
+import { motion } from 'framer-motion';
+import { Star } from "./icons";
+import illustration from "./illustration-thank-you.svg";
 
 
+const App = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const [rating, setRating] = useState(0);
+  return (
+    <>
+      {submitted
+        ? <Thanks rating={rating} />
+        : <Rating
+            setSubmitted={setSubmitted}
+            rating={rating}
+            setRating={setRating}
+          />}
+    </>
+  );
+};
 
-import AnimateText from "./framerMotion/AnimateText"
-import "./App.css"
+const Rating = ({ rating, setRating, setSubmitted }) => {
+  const [btnActive, setBtnActive] = useState();
+  const handleActive = e => {
+    const rateValue = e.target.value;
+    setRating(rateValue);
+    setBtnActive(rateValue);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setSubmitted(true)
+    console.log("submitted");
+  }
+  const btnContainerVariants = {
+    hidden:{},
+    visible:{}
+  }
+  const btnVariants = {
+    hidden:{scale:.3, opacity:0},
+    visible:{scale:1, opacity:1}
+  }
+  return (
+    <div className="rating">
+      <div className="rating__star">
+        <Star />
+      </div>
+      <h2 className="rating__title"> How did we do?</h2>
+      <p className="rating__desc">
+        {" "}Please let us know how we did with your support request. All
+        feedback is appreciated to help us improve our offering!
+      </p>
 
+      <form action="#" onSubmit={handleSubmit}>
+        <motion.div variants={btnContainerVariants} initial="hidden" animate="visible" transition={{staggerChildren:.200}} className="rating__btn-container">
+        <motion.input variants={btnVariants}
+          type="button"
+          onClick={handleActive}
+          value="1"
+          className={btnActive === '1' ? "rating__btn rating__btn--active": "rating__btn"}
+        />
+        <motion.input variants={btnVariants}
+          type="button"
+          onClick={handleActive}
+          value="2"
+          className={btnActive === '2' ? "rating__btn rating__btn--active": "rating__btn"}
+        />
+        <motion.input variants={btnVariants}
+          type="button"
+          onClick={handleActive}
+          value="3"
+          className={btnActive === '3' ? "rating__btn rating__btn--active": "rating__btn"}
+        />
+        <motion.input variants={btnVariants}
+          type="button"
+          onClick={handleActive}
+          value="4"
+          className={btnActive === '4' ? "rating__btn rating__btn--active": "rating__btn"}
+        />
+        <motion.input variants={btnVariants}
+          type="button"
+          onClick={handleActive}
+          value="5"
+          className={btnActive === '5' ? "rating__btn rating__btn--active": "rating__btn"}
+        />
+        </motion.div>
+        <input type="submit" className="rating__submit" disabled={!rating && "disabled"} />
+      </form>
+    </div>
+  );
+};
 
-const App = ()=>{
-  return <AnimateText/>
-}
+const Thanks = ({ rating }) => {
+  return (
+    <motion.div initial={{scale:0}} animate={{scale:1}} className="rating thanks">
+      <object type="image/svg+xml" data={illustration} />
+      <p className="thanks__rated">
+        You selected {rating} out of 5
+      </p>
+      <h2 className="thanks__title">Thank you!</h2>
+      <p className="thanks__desc">
+        We appreciate you taking the time to give a rating. If you ever
+        need more support, don’t hesitate to get in touch!
+      </p>
+    </motion.div>
+  );
+};
 
-export default App
+export default App;
